0fe1a18465079c3794fe14402f0dfe6d43f35dcf
Hope Kenga <97263417+HopeKenga@users.noreply.github.com>
2022-04-21 14:00:47 +0300

    initial

---
+++ portfolio/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React from 'react'
+
+function App() {
+    return ( <
+        main >
+        <
+        Navbar / >
+        <
+        About / >
+        <
+        Projects / >
+        <
+        Skills / >
+        <
+        Testimonials / >
+        <
+        Contacts / >
+        <
+        /main>
+    );
+}
+
+export default App;
