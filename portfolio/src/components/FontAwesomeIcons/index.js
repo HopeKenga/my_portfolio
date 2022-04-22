2985a4cc9b97d822a554a859d5844200f98a45bb
Hope Kenga <97263417+HopeKenga@users.noreply.github.com>
2022-04-22 18:05:21 +0300

    icons

---
+++ portfolio/src/components/FontAwesomeIcons/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/free-brands-svg-icons';
+import { fab } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
