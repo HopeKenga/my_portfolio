28b30e4daf44a852ed2673197b9880d298c09d3a
Hope Kenga <97263417+HopeKenga@users.noreply.github.com>
2022-05-03 13:19:39 +0300

    adding icons

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,66 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'src/components/fontawesome'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaUser } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
+import { FaArrowLeft } from 'react-icons/fa';
+import { FaArrowRight } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
 
 
 export default function About() {
-    return ( <
-        section id = "about" >
-        <
-        div class = "container" >
-        <
-        div class = "strips" >
-        <
-        div class = "s-one" > < /div> <
-        div class = "s-two" > < /div> <
-        div class = "s-three" > < /div> <
-        /div> <
-        div class = "top-bar" >
-        <
-        ul >
-        <
-        li >
-        <
-        div class = "account" >
-        <
-        div style = {
-            { fontSize: '35px' } } >
-        <
-        FontAwesomeIcon icon = "user" / >
-        <
-        FontAwesomeIcon icon = {
-            ['fab', 'facebook-square'] }
-        /> <
-        FontAwesomeIcon icon = {
-            ['fab', 'instagram-square'] }
-        /> <
-        FontAwesomeIcon icon = {
-            ['fab', 'linkedin'] }
-        /> <
-        FontAwesomeIcon icon = {
-            ['fab', 'twitter'] }
-        /> <
-        FontAwesomeIcon icon = {
-            ['fab', 'google'] }
-        /> <
-        FontAwesomeIcon icon = {
-            ['fab', 'youtube'] }
-        /> <
-        FontAwesomeIcon icon = {
-            ['fab', 'skype'] }
-        /> <
-        /div>
+    return (
 
-        <
-        /div> <
-        /li> <
-        /ul>
-
-        <
-        /div> <
-        /div> <
-        /section>
 
     );
 };
