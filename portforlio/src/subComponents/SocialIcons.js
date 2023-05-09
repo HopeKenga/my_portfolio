491ae4800eaff10917d0fa2cc317e5190a873339
Hope Kenga <kengahope7@gmail.com>
2023-05-09 12:25:34 +0300

    chore: fix components

--- portforlio/src/subComponents/SocialIcons.js
+++ portforlio/src/subComponents/SocialIcons.js
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import React from "react";
 // import { NavLink } from 'react-router-dom'
 import styled from "styled-components";
-import { Github, Twitter } from "../components/AllSvgs";
+import { Github, LinkedIn, Twitter } from "../components/AllSvgs";
 import { DarkTheme } from "../components/Themes";
 
 const Icons = styled.div`
@@ -39,7 +39,7 @@ const SocialIcons = (props) => {
         <a
           style={{ color: "inherit" }}
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener noreferrer" 
           href={"https://github.com/HopeKenga"}
         >
           <Github
@@ -67,6 +67,25 @@ const SocialIcons = (props) => {
           />
         </a>
       </motion.div>
+      <motion.div
+        initial={{scale:0 }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1.4 }}
+      >
+        <a
+          style={{ color: "inherit" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href={"https://www.linkedin.com/in/hope-kenga-033108240"}
+        >
+          <LinkedIn
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </a>
+      </motion.div>
+
       <Line
         color={props.theme}
         initial={{
